797970d3d2c87deb9c47597bf042658b305dd99a
Jen <jenodowd@me.com>
2018-04-05 14:06:18 -0400

    update

--- decode/03_javascript-fundamentals/01_expansion/paperproblems/problem1.js
+++ decode/03_javascript-fundamentals/01_expansion/paperproblems/problem1.js
@@ -19,10 +19,38 @@ function reverseString(str) {
 }
 // Expand each of the following and get the result of the expression
 // #1
-square(decrement(square(decrement(3))))
+
+// console.log(square(decrement(square(decrement(3)))))
+
+//     console.log("decrement function: " + decrement(3))
+//     console.log("square function: " + square(2))
+//     console.log("decrement function: " + decrement(4))
+//     console.log("square function: " + square(3))
+       
+       //Answer: 9
+
 // #2
-decrement(decrement(square(square(3))))
+
+// console.log(decrement(decrement(square(square(3)))))
+    
+//     console.log("square function: " + square(3))
+//     console.log("square function: " + square(9))
+//     console.log("decrement function: " + decrement(81))
+//     console.log("decrement function: " + decrement(80))
+
+        //Answer: 79
+
 // #3
-duplicateString(reverseString("hello"))
+
+// console.log(duplicateString(reverseString("hello")))
+
+//     console.log("reverse string: " + reverseString("hello"))
+//     console.log("duplicate string: " + duplicateString("olleh"))
+
 // #4
-reverseString(duplicateString(duplicateString("foo")))
+
+// console.log(reverseString(duplicateString(duplicateString("foo"))))
+
+//     console.log("duplicate string: " + duplicateString("foo"))
+//     console.log("duplicate string: " + duplicateString("foofoo"))
+//     console.log("reverse string: " + reverseString("foofoofoofoo"))
