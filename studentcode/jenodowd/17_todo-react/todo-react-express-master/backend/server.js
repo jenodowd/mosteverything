13492511c3a8a377f37237663b5a9c73a1faf928
Jen <jenodowd@me.com>
2018-05-02 15:45:17 -0400

    update

---
+++ studentcode/jenodowd/17_todo-react/todo-react-express-master/backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.raw({ type: '*/*' }))
+
+let serverState = {
+    items: []
+}
+
+app.get('/items', (req, res) => {
+    res.send(JSON.stringify(serverState.items));
+})
+
+app.post('/addItem', (req, res) => {
+    let parsedBody = JSON.parse(req.body.toString())
+    // The following could be rewritten in a shorter way using push.
+    // I'm trying to ween everyone off of push
+    serverState.items = serverState.items.concat(parsedBody)
+    res.send(JSON.stringify(serverState.items));
+})
+
+app.post('/reverse', (req, res) => {
+    res.send(JSON.stringify(serverState.items.reverse()));
+})
+
+
+
+app.listen(4000, () => console.log('Example app listening on port 4000!'))
